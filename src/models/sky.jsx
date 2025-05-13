f90e66335449786b6d897892c04dedf94c5fc89e
Ranjith.T <121870537+ranjiththangavel2000@users.noreply.github.com>
2025-05-13 12:58:33 +0530

    enhance Bird and Sky models; implement animation and rotation features

--- src/models/sky.jsx
+++ src/models/sky.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
 
 import SkyScene from '../assets/3d/sky.glb'
 
-const sky = () => {
+const sky = ({isRotating}) => {
     const sky = useGLTF(SkyScene);
+    const skyRef = useRef();
+    useFrame((_,delta) => {
+        if (isRotating) {
+            skyRef.current.rotation.y += delta * 0.25;
+        }
+    });
     return (
-        <mesh>
+        <mesh ref={skyRef}>
             <primitive object={sky.scene} />
         </mesh>
     )
